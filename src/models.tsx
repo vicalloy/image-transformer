341374f1f5775a3017eda396215e51742f233f57
vicalloy <zbirder@gmail.com>
2021-10-14 14:25:55 +0800

    add type for ts

--- src/models.tsx
+++ src/models.tsx
@@ -1,4 +1,4 @@
-export const modelConfig = {
+export const modelConfig: { [key: string]: number[] } = {
   'candy': [135, 200, 270, 300, 350, 360, 400, 540, 1200],
   'gogh': [135, 200, 300, 350, 400, 500, 1000, 1500, 4000],
   'rain': [135, 200, 300, 350, 500, 1000],
